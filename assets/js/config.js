529b8d34e0fdbee84e66f29643495e009e0cccb6
mnatucci <156982785+mnatucci@users.noreply.github.com>
2025-12-11 15:19:21 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://nqzfryhjlxzaksblmjtt.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_up_Njp0j-3GWA2eYWHV0hw_FkSA9vH5",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xemZyeWhqbHh6YWtzYmxtanR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NTk3ODksImV4cCI6MjA4MTAzNTc4OX0.QQyBhyXgF-JYx3qxFWRUhucnRbPcVM-OlA9p63RBtT8",
   PIX_KEY: "ccoelhomuniz@gmail.com"
 };

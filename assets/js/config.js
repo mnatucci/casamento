d0b42de4a919226c499135dff09e5ef43fd50fe7
mnatucci <156982785+mnatucci@users.noreply.github.com>
2025-12-27 13:09:37 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-
-window.CONFIG = {
-  SUPABASE_URL: "https://nqzfryhjlxzaksblmjtt.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xemZyeWhqbHh6YWtzYmxtanR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NTk3ODksImV4cCI6MjA4MTAzNTc4OX0.QQyBhyXgF-JYx3qxFWRUhucnRbPcVM-OlA9p63RBtT8",
-  GIFTS_API: "https://script.google.com/macros/s/AKfycbyt5mh7BfdcBugnasxfF6TCfkClBUdfft0X0kNZdN6bX4YMsEXgnW-YAkbe10d4RY32Bw/exec"
-};

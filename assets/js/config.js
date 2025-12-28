54a7c783873716748d4679a9787fb13f7bbbcf62
mnatucci <156982785+mnatucci@users.noreply.github.com>
2025-12-28 18:23:08 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://up-njp0j-3gwa2eywhv0hw-fksa9v-h5.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_up_Njp0j-3GWA2eYWHV0hw_FkSA9vH5",
-  PIX_KEY: "ccoelhomuniz@gmail.com",
-  GIFTS_API: "https://script.google.com/macros/s/AKfycbwNvsLEBFFNQHEWjJWhNVAzLa7vZprl1PmbYbUPw_pQbYI8-DFNnB07gqzz7B9N-7aZ/exec"
-};

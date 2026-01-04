9c109d96f932095dcdb4417ed4ed68898b5b753f
mnatucci <156982785+mnatucci@users.noreply.github.com>
2026-01-04 14:23:49 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw9H6BFZl79DFURIiy1j9_V5XOwMZCa4hZJ4ta-AJCJ7mIADD2KhAPBczFSc81YGTmW/exec",
-  PIX_KEY: "ccoelhomuniz@gmail.com"
-};

ef5bed2184156b231cd5109a739841562f236722
mnatucci <156982785+mnatucci@users.noreply.github.com>
2026-01-04 14:24:50 -0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbw9H6BFZl79DFURIiy1j9_V5XOwMZCa4hZJ4ta-AJCJ7mIADD2KhAPBczFSc81YGTmW/exec",
+  PIX_KEY: "ccoelhomuniz@gmail.com"
+};
